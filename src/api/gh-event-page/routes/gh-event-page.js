53cfce9f15b6415b6be0a991da061c789c61b1da
fjmenendezr <73352031+fjmenendezr@users.noreply.github.com>
2024-10-24 08:38:58 -0300

    feat(single types) add gh event page

---
+++ src/api/gh-event-page/routes/gh-event-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gh-event-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::gh-event-page.gh-event-page');
