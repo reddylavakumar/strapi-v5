554b4c8f63dfaf3b4dc575caee9f301eee19a535
Hasit Mistry <hasit@coderabbit.ai>
2025-01-02 14:11:19 -0800

    add home-page-2 API with controller, service, and schema

---
+++ src/api/home-page-2/controllers/home-page-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-2 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-page-2.home-page-2');
---
+++ src/api/home-page-2/routes/home-page-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-page-2.home-page-2');
