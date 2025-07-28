efda2e75808c7de5995af6e79b6f4d9f0071c20c
Mohammed Khan <mohammed.khan@antstack.io>
2025-07-28 12:11:36 +0530

    feat: add student program API with schema, controller, router, and service

---
+++ src/api/student-program/controllers/student-program.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * student-program controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::student-program.student-program");
